2fdf16da395be1b3447c63dd5a2c7011414d3d67
Jason Felice <jason.m.felice@gmail.com>
2019-05-14 12:13:52 -0400

    Rename to cursor_line

--- scripts/run-editor.sh
+++ scripts/run-editor.sh
@@ -8,8 +8,8 @@ source "$SCRIPTS_DIR/functions.sh"
 file=$(capturePaneContents -t "${pane_id}")
 getPaneProperties cursor_y cursor_x pane_width pane_height
 line_count=$(countLines "$file")
-sel_line=$(( line_count - ( pane_height - cursor_y ) + 1 ))
-cursor="${sel_line}.${cursor_x},${sel_line}.${cursor_x}"
+cursor_line=$(( line_count - ( pane_height - cursor_y ) + 1 ))
+cursor="${cursor_line}.${cursor_x},${cursor_line}.${cursor_x}"
 kak -e "
    edit $file
    exec gj
